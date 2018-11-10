f3b9441941e6212e9727b6aa19cb389f02f7e830
harry10148 <harry10148@gmail.com>
2018-11-10 21:47:21 +0800

    aws

--- scripts/BeforeInstall.sh
+++ scripts/BeforeInstall.sh
@@ -4,11 +4,11 @@ echo "Logging in to AWS ECR and pull images"
 $(aws ecr get-login --no-include-email --region ap-northeast-1)
 docker pull 204065533127.dkr.ecr.ap-northeast-1.amazonaws.com/api:latest
 docker pull 204065533127.dkr.ecr.ap-northeast-1.amazonaws.com/jupyter:latest
-docker pull 204065533127.dkr.ecr.ap-northeast-1.amazonaws.com/ngrok:latest
 docker pull mysql:latest
 docker pull redis:latest
+docker pull nginx:latest
 cd /home/ec2-user/deploy/Chatbot_Line_cc103/code
-aws s3 cp s3://cc103-deploy-certification/secret_key.txt .
+aws s3 cp s3://cc103-deploy-certification/secret_key .
 cd /home/ec2-user/deploy/Chatbot_Line_cc103/docker_vol/cert-letsencrypt/
 aws s3 cp s3://cc103-deploy-certification/cert-letsencrypt/cc103awsbot.crt .
 aws s3 cp s3://cc103-deploy-certification/cert-letsencrypt/cc103awsbot.key .
